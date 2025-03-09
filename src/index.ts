1dd4bb9f0a27986033a2fd973b4c8bce61316a4e
santhosh <santhosh.inhyd@gmail.com>
2025-03-10 01:26:24 +0530

    Using commaand line argument for username

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,8 @@ if (process.argv.length < 3) {
     console.log('Please enter a valid Github username');
 } else {
     console.log('Github User: '  + process.argv[2]);
-    githubAPIService.getUserData('SanthoshVeera', (user) => {
-        githubAPIService.getAllRepos('SanthoshVeera', (repos) => {
+    githubAPIService.getUserData(process.argv[2], (user) => {
+        githubAPIService.getAllRepos(process.argv[2], (repos) => {
             let sortedRepos: Repo[] = _.sortBy(repos, [(repo) => repo.noOfForks * -1]);
             let filteredRepos: Repo[] = _.take(sortedRepos, 3);
             user.repos = filteredRepos;
